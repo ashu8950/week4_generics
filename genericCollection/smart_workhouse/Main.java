7ac11b9aa9273bc0a4204e719af1738987adaa7b
Ashu <shabsingh961@gmail.com>
2025-03-19 14:17:30 +0530

    [Ashu] Refractor class name SmartWorkHouseSystem -> Main

---
+++ genericCollection/smart_workhouse/Main.java
@@ -0,0 +1,29 @@
+package genericCollection.smart_workhouse;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Storage<Electronics> electronicsStorage = new Storage<>();
+        electronicsStorage.addItem(new Electronics("Smartphone"));
+        electronicsStorage.addItem(new Electronics("Laptop"));
+
+        Storage<Groceries> groceriesStorage = new Storage<>();
+        groceriesStorage.addItem(new Groceries("Apples"));
+        groceriesStorage.addItem(new Groceries("Milk"));
+
+        Storage<Furniture> furnitureStorage = new Storage<>();
+        furnitureStorage.addItem(new Furniture("Chair"));
+        furnitureStorage.addItem(new Furniture("Table"));
+
+        System.out.println("All Electronics:");
+        WarehouseUtils.displayItems(electronicsStorage.getAllItem());
+
+        System.out.println("\nAll Groceries:");
+        WarehouseUtils.displayItems(groceriesStorage.getAllItem());
+
+        System.out.println("\nAll Furniture:");
+        WarehouseUtils.displayItems(furnitureStorage.getAllItem());
+	}
+
+}
